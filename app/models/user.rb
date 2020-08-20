061f4fb6fabc9485597d904ee396b0f2706af728
aracelicaes <aracelicaes@gmail.com>
2020-08-20 12:09:47 -0500

    Create Post Model Generated Post Model Create Posts table: title, body, user_id Added Post validations to ensure there's a user assigned to each post. Body length doesn't exceed 200 characters. Added Associations Between User and Post. :

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
 end
