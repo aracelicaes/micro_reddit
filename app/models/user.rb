789333d5301f2cb25583c8d3826473cc6a236b59
aracelicaes <aracelicaes@gmail.com>
2020-08-20 12:52:06 -0500

    Adding Linters Added Rubocop Added Linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
+  has_many :posts
+  has_many :comments
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
 end
